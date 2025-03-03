071d05d6a3f9c2f99c116cd1c3cff013d611e2b5
mym <rei1gggg@gmail.com>
2025-03-03 23:27:58 +0900

    tanstack install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
+import { QueryProvider } from "@/components/query-provider";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -21,7 +22,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={cn(inter.className, "antialiased min-h-screen")}>
-				{children}
+				<QueryProvider>{children}</QueryProvider>
 			</body>
 		</html>
 	);
