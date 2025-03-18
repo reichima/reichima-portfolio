dfc6b28bfa085d3e961a1b1d097b90cabddf32c5
mym <rei1gggg@gmail.com>
2025-03-18 23:44:16 +0900

    ログインまで

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,25 +1,41 @@
+import { AUTH_COOKIE } from "@/features/auth/constants";
 import { createAdminClient } from "@/lib/appwrite";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { setCookie } from "hono/cookie";
 import { ID } from "node-appwrite";
 import { loginSchema, registerSchema } from "../schemas";
 
 const app = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = await c.req.valid("json");
-    console.log(email, password);
+    const { account } = await createAdminClient();
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
     return c.json({
-      message: "Login successful!",
+      success: true,
     });
   })
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = await c.req.valid("json");
     const { account } = await createAdminClient();
-    const user = await account.create(ID.unique(), email, password, name);
+    await account.create(ID.unique(), email, password, name);
     const session = await account.createEmailPasswordSession(email, password);
-    console.log(name, email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
     return c.json({
-      message: "Registration successful!",
+      success: true,
     });
   });
 
--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -4,7 +4,7 @@ import "server-only";
 export async function createAdminClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
     .setKey(process.env.NEXT_APPWRITE_KEY!);
 
   return {
