76fd37de67a9a2a3ca1c0fa65067321b0b9096c2
reichima <rei1gggg@gmail.com>
2025-06-05 15:28:23 +0900

    vercel設定

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,6 +3,7 @@ import contact from "@/features/contact/server/route";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+export const runtime = "edge";
 const app = new Hono().basePath("/api");
 
 // ルートを正しく初期化
