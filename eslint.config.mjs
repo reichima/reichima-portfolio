4a8fcb56786a7a526812efd5fca3a25d7a1fbfb8
reichima <rei1gggg@gmail.com>
2025-06-05 15:25:52 +0900

    eslintignore削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +10,27 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "node_modules/",
+      ".next/",
+      ".nuxt/",
+      ".astro/",
+      "build/",
+      "dist/",
+      "out/",
+      "public/",
+      "package-lock.json",
+      "yarn.lock",
+      "pnpm-lock.yaml",
+      "vite.config.ts",
+      "next.config.js",
+      "tsconfig.json",
+      "src/env.d.ts",
+      "*.cjs",
+      "*.mjs",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
