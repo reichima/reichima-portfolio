94ae6a03b9e316ac312d01ffac6bc88bcc3c0dae
reichima <rei1gggg@gmail.com>
2025-06-17 00:02:53 +0900

    新規登録ボタン削除

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,34 +1,27 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 interface AuthLayoutProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-	const pathname = usePathname();
-	const isSignIn = pathname === "/sign-in";
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
 
-	return (
-		<main className="bg-neutral-100 min-h-screen">
-			<div className="mx-auto max-w-(--breakpoint-2xl) p-4">
-				<nav className="flex justify-between items-center">
-					<Image src="/flog.png" alt="カエルロゴ" width={60} height={60} />
-					<Button variant="secondary">
-						<Link href={isSignIn ? "/sign-up" : "/sign-in"}>
-							{isSignIn ? "新規登録" : "ログイン"}
-						</Link>
-					</Button>
-				</nav>
-				<div className="flex flex-col justify-center items-center pt-4 md:pt-8">
-					{children}
-				</div>
-			</div>
-		</main>
-	);
+  return (
+    <main className="min-h-screen bg-neutral-100">
+      <div className="mx-auto max-w-(--breakpoint-2xl) p-4">
+        <nav className="flex items-center justify-between">
+          <Image src="/flog.png" alt="カエルロゴ" width={60} height={60} />
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-8">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default AuthLayout;
